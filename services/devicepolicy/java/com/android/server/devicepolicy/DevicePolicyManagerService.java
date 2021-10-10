831a15fadc1bc8390b997057e7b22eb8758ed8a8
Oliver Scott <olivercscott@gmail.com>
2021-10-09 20:16:19 -0400

    Firewall: Network access toggle support
    Everything needed to support prohibiting apps' overall network access,
    apart from changes for Settings / Firewall app.
    
    "Firewall: Migrate to POLICY_REJECT_ALL" is critical to include, too,
    if existing installations with prior implementations must be supported.
    
    Update restriction rules for an app when it is being updated/replaced,
    not only when it is installed for the first time. Use the latest data
    when checking for internet permission.
    
    Co-authored-by: Chirayu Desai <chirayudesai1@gmail.com>
    Co-authored-by: Tommy Webb <tommy@calyxinstitute.org>
    Change-Id2: I608a792e54cad347f4c256029a2959d6f6b45e3b
    Change-Id: I5fae5389776196175eba31f646efff6771824dcc
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
+++ services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
@@ -22069,7 +22069,7 @@ public class DevicePolicyManagerService extends IDevicePolicyManager.Stub {
                 }
             }
 
-            onCreateAndProvisionManagedProfileCompleted(provisioningParams);
+            onCreateAndProvisionManagedProfileCompleted(userInfo.id, provisioningParams);
 
             sendProvisioningCompletedBroadcast(
                     userInfo.id,
@@ -22209,7 +22209,8 @@ public class DevicePolicyManagerService extends IDevicePolicyManager.Stub {
             @NonNull ManagedProfileProvisioningParams provisioningParams,
             @NonNull UserHandle managedProfileUser
     ) {
-        onCreateAndProvisionManagedProfileCompleted(provisioningParams);
+        onCreateAndProvisionManagedProfileCompleted(managedProfileUser.getIdentifier(),
+                provisioningParams);
         sendProvisioningCompletedBroadcast(
                 managedProfileUser.getIdentifier(),
                 ACTION_PROVISION_MANAGED_PROFILE,
@@ -22265,8 +22266,17 @@ public class DevicePolicyManagerService extends IDevicePolicyManager.Stub {
      *
      * <p>This method is meant to be overridden by OEMs.
      */
-    private void onCreateAndProvisionManagedProfileCompleted(
-            ManagedProfileProvisioningParams provisioningParams) {}
+    private void onCreateAndProvisionManagedProfileCompleted(int userId,
+            ManagedProfileProvisioningParams provisioningParams) {
+        try {
+            Set<Integer> uids = ConnectivitySettingsManager.getUidsAllowedOnRestrictedNetworks(
+                    mContext);
+            uids.add(mContext.getPackageManager().getPackageUidAsUser(
+                    provisioningParams.getOwnerName(), userId));
+            ConnectivitySettingsManager.setUidsAllowedOnRestrictedNetworks(mContext, uids);
+        } catch (NameNotFoundException ignored) {
+        }
+    }
 
     private void maybeInstallDevicePolicyManagementRoleHolderInUser(@UserIdInt int targetUserId,
             CallerIdentity caller) {
