2a93ce25100b1cd09da505094db294cd3b3b62f6
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-09-14 08:22:36 +0530

    SystemUI: Move keyguard date slice row to a separate function
    Make it accessible to derived classes.
    
    Change-Id: I275475dcd825a3fe6917db73fd33d5c59a55caea
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardSliceProvider.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardSliceProvider.java
@@ -223,7 +223,7 @@ public class KeyguardSliceProvider extends SliceProvider implements
                 if (needsMediaLocked()) {
                     addMediaLocked(builder);
                 } else {
-                    builder.addRow(new RowBuilder(mDateUri).setTitle(mLastText));
+                    addDateLocked(builder);
                 }
                 addNextAlarmLocked(builder);
                 addZenModeLocked(builder);
@@ -236,6 +236,10 @@ public class KeyguardSliceProvider extends SliceProvider implements
             Trace.endSection();
         }
         return slice;
+    }
+    
+    protected void addDateLocked(ListBuilder builder) {
+        builder.addRow(new RowBuilder(mDateUri).setTitle(mLastText));
     }
 
     protected boolean needsMediaLocked() {
@@ -264,7 +268,6 @@ public class KeyguardSliceProvider extends SliceProvider implements
             if (mediaIconCompat != null) {
                 albumBuilder.addEndItem(mediaIconCompat, ListBuilder.ICON_IMAGE);
             }
-            albumBuilder.setEndOfSection(true);
 
             listBuilder.addRow(albumBuilder);
         }
@@ -290,8 +293,7 @@ public class KeyguardSliceProvider extends SliceProvider implements
                 R.drawable.ic_access_alarms_big);
         RowBuilder alarmRowBuilder = new RowBuilder(mAlarmUri)
                 .setTitle(mNextAlarm)
-                .addEndItem(alarmIcon, ListBuilder.ICON_IMAGE)
-                .setEndOfSection(true);
+                .addEndItem(alarmIcon, ListBuilder.ICON_IMAGE);
         builder.addRow(alarmRowBuilder);
     }
 
@@ -308,8 +310,7 @@ public class KeyguardSliceProvider extends SliceProvider implements
                         .getString(R.string.accessibility_quick_settings_dnd))
                 .addEndItem(
                     IconCompat.createWithResource(getContext(), R.drawable.stat_sys_dnd),
-                    ListBuilder.ICON_IMAGE)
-                .setEndOfSection(true);
+                    ListBuilder.ICON_IMAGE);
         builder.addRow(dndBuilder);
     }
 
