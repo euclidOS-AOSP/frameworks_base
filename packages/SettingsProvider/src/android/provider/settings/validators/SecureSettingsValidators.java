ddb1b5ba17917ea1e2bd21f1bd983ac6a5f78279
Abhay Singh Gill <abhaygill017@gmail.com>
2025-05-29 03:59:29 +0530

    base: Add key generation for keybox spoofing [1/2]
    The code is used from :-
    https://github.com/5ec1cff/TrickyStore
    
    This allows us to enjoy spoofing keyboxes the right way even with broken TEE.
    Allow users to provide keybox XML as well.
    
    Change-Id: I0da036c907f8cd1f6d2e3480cf3d5f688ac17dc0
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/validators/SecureSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SecureSettingsValidators.java
@@ -527,5 +527,6 @@ public class SecureSettingsValidators {
                 new InclusiveIntegerRangeValidator(0, 2));
         VALIDATORS.put(Secure.PIF_DATA, ANY_STRING_VALIDATOR);
         VALIDATORS.put(Secure.FETCHED_PIF, ANY_STRING_VALIDATOR);
+        VALIDATORS.put(Secure.KEYBOX_DATA, ANY_STRING_VALIDATOR);
     }
 }
