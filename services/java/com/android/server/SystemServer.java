32a65272199fa33d47d115aae7e7233f9bbc62ae
Pranav Vashi <neobuddy89@gmail.com>
2025-10-27 11:36:41 +0530

    base: Auto set vbmeta digest and other props [1/2]
    * These are set if not available from bootloader. Fixes abnormal boot state detection.
    * This service uses key attestation code from
      https://github.com/reveny/Android-VBMeta-Fixer
      https://github.com/vvb2060/KeyAttestation
    * Second part is in property_service.cpp of system/core to read persist prop
      to set hash value and other props.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -169,6 +169,7 @@ import com.android.server.contextualsearch.ContextualSearchManagerService;
 import com.android.server.coverage.CoverageService;
 import com.android.server.cpu.CpuMonitorService;
 import com.android.server.crashrecovery.CrashRecoveryAdaptor;
+import com.android.server.euclid.VbmetaHashService;
 import com.android.server.credentials.CredentialManagerService;
 import com.android.server.criticalevents.CriticalEventLog;
 import com.android.server.devicepolicy.DevicePolicyManagerService;
@@ -2882,6 +2883,11 @@ public final class SystemServer implements Dumpable {
 
             t.traceBegin("StartHealthService");
             mSystemServiceManager.startService(HealthInterfaceService.class);
+            t.traceEnd();
+
+            // VbmetaHashService
+            t.traceBegin("VbmetaHashService");
+            mSystemServiceManager.startService(VbmetaHashService.class);
             t.traceEnd();
         }
 
