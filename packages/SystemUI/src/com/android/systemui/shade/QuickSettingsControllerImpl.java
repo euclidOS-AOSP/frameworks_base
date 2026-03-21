726f95ae03bfa63c2665c1834642189f7ee15bdf
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-03-22 07:11:54 +0800

    core: Adding sandbox support
    credit
    HMA OSS | Furkan Karcıoğlu
    
    Co-authored-by: Furkan Karcıoğlu <45714956+frknkrc44@users.noreply.github.com>
    Change-Id: I4531975e10894b46807db7c3fdebf0203ffbca77
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/QuickSettingsControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/shade/QuickSettingsControllerImpl.java
@@ -108,6 +108,12 @@ import com.android.systemui.util.LargeScreenUtils;
 import com.android.systemui.util.kotlin.JavaAdapter;
 import com.android.systemui.utils.windowmanager.WindowManagerProvider;
 
+import com.android.systemui.applocker.AxAppLockerHelper;
+import com.android.systemui.statusbar.notification.row.ExpandableNotificationRow;
+import com.android.systemui.statusbar.notification.collection.NotificationEntry;
+import android.service.notification.StatusBarNotification;
+import android.view.View;
+
 import dalvik.annotation.optimization.NeverCompile;
 
 import dagger.Lazy;
@@ -2475,5 +2481,15 @@ public class QuickSettingsControllerImpl implements QuickSettingsController, Dum
     interface FlingQsWithoutClickListener {
         void onFlingQsWithoutClick(ValueAnimator animator, float qsExpansionHeight,
                 float target, float vel);
+    }
+
+    public final void onAppLockerUpdated(String packageName) {
+        NotificationStackScrollLayoutController controller = mNotificationStackScrollLayoutController;
+        if (controller == null || controller.getView() == null) {
+            return;
+        }
+
+        NotificationStackScrollLayout view = controller.getView();
+        view.post(() -> view.onAppLockerUpdate(packageName));
     }
 }
--- services/core/java/com/android/server/wm/RecentTasks.java
+++ services/core/java/com/android/server/wm/RecentTasks.java
@@ -2049,6 +2049,7 @@ class RecentTasks {
         if (!getTasksAllowed) {
             Task.trimIneffectiveInfo(tr, rti);
         }
+        AxSandboxService.get().isTopAppLocked(rti, tr.effectiveUid);
         return rti;
     }
 
