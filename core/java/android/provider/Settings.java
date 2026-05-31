b331e0d11edbb17213a1266b5a78aa5e07b92c53
MODEDGES <ofwargane30@gmail.com>
2026-05-31 20:32:47 +0100

    fb: Add missing NETWORK_TRAFFIC_* settings to Secure class in Settings.java to fix build failure in NetworkTraffic.java.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6916,34 +6916,7 @@ public final class Settings {
          * @hide
          */
         public static final String QS_TILE_SHAPE = "qs_tile_shape";
-        
-         /**
-         * Network traffic indicator mode
-         * 0 = Don't show network traffic indicator
-         * 1 = Display up-stream traffic only
-         * 2 = Display down-stream traffic only
-         * 3 = Display both up- and down-stream traffic
-         * @hide
-         */
-        public static final String NETWORK_TRAFFIC_MODE = "network_traffic_mode";
 
-        /**
-         * Whether or not to hide the network traffic indicator when there is no activity
-         * @hide
-         */
-        public static final String NETWORK_TRAFFIC_AUTOHIDE = "network_traffic_autohide";
-
-        /**
-         * Measurement unit preference for network traffic
-         * 0 = kBit/s
-         * 1 = MBit/s
-         * 2 = kByte/s
-         * 3 = MByte/s
-         * 4 = automatic kByte/s or MByte/s
-         * @hide
-         */
-        public static final String NETWORK_TRAFFIC_UNITS = "network_traffic_units";
-        
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
@@ -9175,6 +9148,30 @@ public final class Settings {
          */
         public static final String SUGGESTED_THEME_FEATURE_ENABLED =
                 "suggested_theme_feature_enabled";
+
+         /**
+         * Setting to indicate that network traffic mode.
+         * 0 = Off
+         * 1 = Upload
+         * 2 = Download
+         * 3 = Both
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_MODE = "network_traffic_mode";
+
+        /**
+         * Setting to indicate that network traffic autohide.
+         * 0 = Off
+         * 1 = On
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_AUTOHIDE = "network_traffic_autohide";
+
+        /**
+         * Setting to indicate that network traffic units.
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_UNITS = "network_traffic_units";
 
         /**
          * String property which is a comma separated list of package names. These package names
