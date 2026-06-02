5fb51eaf8cab2cfd121e73e0277faf09c6adc906
MODEDGES <ofwargane30@gmail.com>
2026-06-02 10:43:45 +0100

    fb: Move shouldShowFourgIcon implementation from trackedCommonViewModel to trackedInteractor to match the interface definitions.

--- packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/ui/viewmodel/MobileIconsViewModelKairos.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/ui/viewmodel/MobileIconsViewModelKairos.kt
@@ -169,6 +169,7 @@ constructor(
                 latest(false) { isAllowedDuringAirplaneMode }
             override val carrierNetworkChangeActive: State<Boolean> =
                 latest(false) { carrierNetworkChangeActive }
+            override val shouldShowFourgIcon: State<Boolean> = latest(false) { shouldShowFourgIcon }
         }
 
     private fun trackedCommonViewModel(subId: Int) =
@@ -194,7 +195,6 @@ constructor(
             override val activityOutVisible: State<Boolean> = latest(false) { activityOutVisible }
             override val activityContainerVisible: State<Boolean> =
                 latest(false) { activityContainerVisible }
-            override val shouldShowFourgIcon: State<Boolean> = latest(false) { shouldShowFourgIcon }
         }
 
     private fun BuildScope.commonViewModel(subId: Int, iconInteractor: MobileIconInteractorKairos) =
